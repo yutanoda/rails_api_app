dac0008684e5a0c7d38259212dad9f88e3908142
y-noda <y-noda@entaworks.co.jp>
2021-07-13 18:19:17 +0900

    redisの環境構築

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -57,6 +57,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
+  config.cache_store = :redis_store, 'redis://redis:6379/0', { expires_in: 1.minute }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
