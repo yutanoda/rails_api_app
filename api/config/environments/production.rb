0a971898abbb41905ea120c06ecc2b0efece5ea3
y-noda <y-noda@entaworks.co.jp>
2021-07-19 19:05:01 +0900

    [fix]redisの設定の見直し

--- api/config/environments/production.rb
+++ api/config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
-  config.cache_store = :redis_store, 'redis://redis:6379/0', { expires_in: 1.minute }
+  config.cache_store = :redis_cache_store, { url: 'redis://redis:6379/0' }
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
